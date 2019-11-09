c42c3cc0aac6846d3277a26428503f2364a1969f
allison cortez <allizetorc@gmail.com>
2019-11-09 04:10:24 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,10 +5,10 @@ class Dancer
   extend MetaDancing
   # include Dance 
   
-  attr_accessor :name 
+  # attr_accessor :name 
   
-  def initialize(name)
-    @name = name 
-  end
+  # def initialize(name)
+  #   @name = name 
+  # end
   
 end
